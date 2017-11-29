266afe544f28f372666f8dec4008d17b02edce51
Beth Massi <bethma@microsoft.com>
2017-11-29 14:58:27 -0800

    Add ServiceCompanies

---
+++ HAMWeb/Controllers/ServiceCompaniesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HAMWeb.Models;
+
+namespace HAMWeb.Controllers
+{
+    public class ServiceCompaniesController : Controller
+    {
+        private readonly HAMDataContext _context;
+
+        public ServiceCompaniesController(HAMDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ServiceCompanies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ServiceCompanies.ToListAsync());
+        }
+
+        // GET: ServiceCompanies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceCompanies = await _context.ServiceCompanies
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceCompanies == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceCompanies);
+        }
+
+        // GET: ServiceCompanies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServiceCompanies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ContactName,Phone,CreatedBy,Created,ModifiedBy,Modified,RowVersion")] ServiceCompanies serviceCompanies)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(serviceCompanies);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceCompanies);
+        }
+
+        // GET: ServiceCompanies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceCompanies = await _context.ServiceCompanies.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceCompanies == null)
+            {
+                return NotFound();
+            }
+            return View(serviceCompanies);
+        }
+
+        // POST: ServiceCompanies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ContactName,Phone,CreatedBy,Created,ModifiedBy,Modified,RowVersion")] ServiceCompanies serviceCompanies)
+        {
+            if (id != serviceCompanies.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(serviceCompanies);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServiceCompaniesExists(serviceCompanies.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceCompanies);
+        }
+
+        // GET: ServiceCompanies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceCompanies = await _context.ServiceCompanies
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceCompanies == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceCompanies);
+        }
+
+        // POST: ServiceCompanies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var serviceCompanies = await _context.ServiceCompanies.SingleOrDefaultAsync(m => m.Id == id);
+            _context.ServiceCompanies.Remove(serviceCompanies);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiceCompaniesExists(int id)
+        {
+            return _context.ServiceCompanies.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HAMWeb/api/ServiceCompaniesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HAMWeb.Models;
+
+namespace HAMWeb.api
+{
+    [Produces("application/json")]
+    [Route("api/ServiceCompanies")]
+    public class ServiceCompaniesController : Controller
+    {
+        private readonly HAMDataContext _context;
+
+        public ServiceCompaniesController(HAMDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ServiceCompanies
+        [HttpGet]
+        public IEnumerable<ServiceCompanies> GetServiceCompanies()
+        {
+            return _context.ServiceCompanies;
+        }
+
+        // GET: api/ServiceCompanies/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetServiceCompanies([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var serviceCompanies = await _context.ServiceCompanies.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (serviceCompanies == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(serviceCompanies);
+        }
+
+        // PUT: api/ServiceCompanies/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutServiceCompanies([FromRoute] int id, [FromBody] ServiceCompanies serviceCompanies)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != serviceCompanies.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(serviceCompanies).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServiceCompaniesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ServiceCompanies
+        [HttpPost]
+        public async Task<IActionResult> PostServiceCompanies([FromBody] ServiceCompanies serviceCompanies)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.ServiceCompanies.Add(serviceCompanies);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetServiceCompanies", new { id = serviceCompanies.Id }, serviceCompanies);
+        }
+
+        // DELETE: api/ServiceCompanies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteServiceCompanies([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var serviceCompanies = await _context.ServiceCompanies.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceCompanies == null)
+            {
+                return NotFound();
+            }
+
+            _context.ServiceCompanies.Remove(serviceCompanies);
+            await _context.SaveChangesAsync();
+
+            return Ok(serviceCompanies);
+        }
+
+        private bool ServiceCompaniesExists(int id)
+        {
+            return _context.ServiceCompanies.Any(e => e.Id == id);
+        }
+    }
+}
